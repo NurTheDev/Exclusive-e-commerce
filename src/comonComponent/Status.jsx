d7ca88611ecab100c0b2ff2eef683d72c0c0af34
NurTheDev <nur756.islam@gmail.com>
2025-06-24 01:06:13 +0600

    added status bar

--- src/comonComponent/Status.jsx
+++ src/comonComponent/Status.jsx
@@ -2,15 +2,51 @@ import React from 'react';
 
 const Status = () => {
     return (
-        <div className={"bg-black text-white flex justify-end"}>
-            <div className={"max-w-2/3 flex items-center justify-between"}>
-               <div className={"flex gap-x-2 p-2"}>
-                   <p>
-                       Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
-                   </p>
-                   <button className={"small-text font-semibold underline cursor-pointer"}> Shop Now</button>
-               </div>
+        <div className={"bg-black text-white flex justify-end "}>
+            <div className={"container"}>
+                <div className={"max-w-2/3 flex items-center justify-between w-full"}>
+                    <div className={"flex gap-x-2 p-2"}>
+                        <p>
+                            Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+                        </p>
+                        <button className={"small-text font-semibold underline cursor-pointer"}> Shop Now</button>
+                    </div>
 
+                    <div>
+                        <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown"
+                                className="text-white focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center cursor-pointer"
+                                type="button">Language <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true"
+                                                            xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                            viewBox="0 0 10 6">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                  d="m1 1 4 4 4-4"/>
+                        </svg>
+                        </button>
+                        <div id="dropdown"
+                             className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700">
+                            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                                aria-labelledby="dropdownDefaultButton">
+                                <li>
+                                    <a href="#"
+                                       className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Bangla</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                       className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">English</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                       className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Hindi</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                       className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Arabic </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     );
