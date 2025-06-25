8be82490a8d5526a2cd11cac0e6afb99b0a97180
NurTheDev <nur756.islam@gmail.com>
2025-06-26 00:30:17 +0600

    added banner with responsive

--- src/Pages/protected/home/Banner.jsx
+++ src/Pages/protected/home/Banner.jsx
@@ -4,27 +4,20 @@ import {MdArrowForwardIos} from "react-icons/md";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import {getBannerImg} from "../../../utils/index.js";
+import {getBannerImg, getSettings} from "../../../utils/index.js";
 const Banner = () => {
     const [openIndex, setOpenIndex] = useState(null)
     const toggle = (index) => {
         setOpenIndex(openIndex === index ? null : index);
     };
     // slider settings
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-    };
+const settings = getSettings()
     return (
         <div className={"container mx-auto"}>
             <div >
-                <div className={"grid grid-cols-4"}>
+                <div className={"lg:grid grid-cols-4"}>
                     {/*Menu bar start*/}
-                    <ul className={"menu  rounded-box w-56 font-poppins"}>
+                    <ul className={"menu rounded-box w-56 font-poppins w-full"}>
                         {menuItems.map((item, index) => (
                             <li key={index}>
                                 <button
@@ -52,17 +45,20 @@ const Banner = () => {
                     </ul>
                     {/*Menu bar start*/}
                     {/*    Carousel Image start*/}
-                    <div className={"col-span-3 "}>
+                    <div className={"col-span-3 px-2 lg:px-0"}>
                         <div className="slider-container">
                             <Slider {...settings}>
                                 {bannerData.map((item, index) => (
-                                    <div key={index} className="w-full h-[350px] !flex !justify-around bg-black">
-                                        <div className={" flex flex-col justify-center gap-y-10"}>
+                                    <div key={index} className="w-full relative h-[250px] lg:h-[350px] !flex !justify-around bg-black">
+                                        <div className={"absolute p-4 lg:p-10 lg:relative top-0 left-0 w-full h-full" +
+                                            " flex" +
+                                            " flex-col" +
+                                            " justify-center gap-y-10 items-start"}>
                                             <p className={"text-white normal-text"}>{item.name}</p>
                                             <h2 className={"text-white xl-heading-semi-bold"}>Up to {item.voucher} off</h2>
-                                            <button className={"btn "}>Shop Now</button>
+                                            <button className={"btn lg:w-1/2"}>Shop Now</button>
                                         </div>
-                                        <img src={getBannerImg(item.image)} alt={item.name} className="w-1/2 object-cover" />
+                                        <img src={getBannerImg(item.image)} alt={item.name} className="lg:w-1/2 object-cover" />
                                     </div>
                                 ))}
                             </Slider>
