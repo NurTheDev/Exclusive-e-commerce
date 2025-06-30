6c5af8a55f33d013f7a5d6fb1a3eaa541d567af1
NurTheDev <nur756.islam@gmail.com>
2025-06-30 16:13:06 +0600

    working on new Arrival

--- src/Pages/protected/home/Component/NewArrivals.jsx
+++ src/Pages/protected/home/Component/NewArrivals.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 const NewArrivals = () => {
     return (
         <div className={"container mx-auto"}>
-
+            <div className={"grid grid-cols-1 lg:grid-cols-2 gap-5"}>
+                <div>
+                    <div>
+                        <img src="" alt=""/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
